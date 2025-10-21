bf2baf4fa838f513f392e09a952b28b6c4cb052b
NobleWolf42 <greenboyb1@gmail.com>
2025-10-21 10:45:48 -0400

    first IP assignment

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#Defining main function
+def main():
+    #Prints "Hello World" to console
+    print("Hello World")
+
+    #Gets user's name from input and stores it in the name variable
+    name = input("Enter your name: ")
+    
+    #Prints "Hello " followed by the name stroed in the name variable
+    print(f"Hello {name}")
+
+if __name__ == "__main__":
+    main()
